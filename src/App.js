1cd78a05b59a07acbf0fc94138517c0abddbeb66
Subhashree naik <subhashree.naik@gmail.com>
2022-04-24 22:55:37 +0530

    Demonstating event binding technique

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import logo from './logo.png';
 import './App.css';
+import React from 'react';
 
 function App() {
+  const url="https://www.bridgelabz.com/";
 var message="Hello from BridgeLabz";
+
+//Event Binding technique
+function onClicked(Event){
+  
+  console.log("button is clicked",Event);
+  window.open(url,"open")
+}
 
   return (
     //Demonstrating  property binding
     <div className="App">
       <h1>{message}</h1> 
-      <img src={logo} alt='bridgeLabz logo'/>
+      <img src={logo} alt='bridgeLabz logo' onClick={onClicked(Event)}/>
+
     </div>
   );
 }
