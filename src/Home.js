d1d9ab7a3278238a3017955e4cb00c6f13725730
Subhashree naik <subhashree.naik@gmail.com>
2022-04-25 15:03:35 +0530

    Demonstrating React router dom

---
+++ src/Home.js
@@ -0,0 +1,16 @@
+
+import React from 'react';
+
+function Home() {
+    var message="Hello from BridgeLabz";
+    
+      return (
+        //Demonstrating  property binding
+        <div className="App">
+          <h1>{message}</h1> 
+          
+        </div>
+      );
+    }
+    
+    export default Home;
---
+++ src/Main.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Main() {
+    var message="Hello from BridgeLabz";
+    
+      return (
+        //Demonstrating  property binding
+        <div className="Main">
+          <h1>{message}</h1> 
+          
+        </div>
+      );
+    }
+    
+    export default Main;
